7b448f2f1ce18dfc8a100b31f63fa0efb4bb0b08
Dawit Hagos <dawhagos@gmail.com>
2022-06-08 03:58:42 -0700

    implemented jsoup scraper on ESPN NBA efficiency rankings.

--- src/main/java/WebScrape/EspnScrape.java
+++ src/main/java/WebScrape/EspnScrape.java
@@ -1,7 +1,30 @@
 package WebScrape;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class EspnScrape {
     public static void main(String[] args) {
-
+        final String url = "http://www.espn.com/nba/hollinger/statistics";
+        try {
+            final Document document = Jsoup.connect(url).get();
+            Elements body = document.select("table.tablehead tr");
+            for (Element row : body) {
+                if (row.select("td:nth-of-type(2)").text().equals("") || row.select("td:nth-of-type(2)").text().equals("PLAYER")) {
+                    continue;
+                } else {
+                    final String player = row.select("td:nth-of-type(2)").text();
+                    final String gp = row.select("td:nth-of-type(3)").text();
+                    final String per = row.select(".sortcell").text();
+                    final int gamesPlayed = Integer.parseInt(gp);
+                    final double playerEfficiency = Double.parseDouble(per);
+                    System.out.println(player + "  " + gp + "  " + playerEfficiency);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
