5dbe2908ae6b027dbec88a587ead7d64068145b7
Dawit Hagos <dawhagos@gmail.com>
2022-10-14 16:07:18 -0700

    added error page and removed swagger-ui

--- src/main/java/com/hagos/WebScrape/controller/EspnScrapeController.java
+++ src/main/java/com/hagos/WebScrape/controller/EspnScrapeController.java
@@ -1,7 +1,6 @@
 package com.hagos.WebScrape.controller;
 
 import com.hagos.WebScrape.service.EspnScrapeService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @Log
 @RequestMapping(path = "/")
-@Tag(name = "2021-22 Hollinger NBA Player Statistics")
 public class EspnScrapeController {
     final private EspnScrapeService espnScrapeService;
 
@@ -27,14 +25,14 @@ public class EspnScrapeController {
     @GetMapping(value = "players")
     public String showAllPlayers(Model model) {
         log.info("[/players] GET endpoint invoked");
-        model.addAttribute("allplayers", espnScrapeService.playersExtractData());
+        model.addAttribute("allPlayers", espnScrapeService.playersExtractData());
         return "players";
     }
 
     @GetMapping(value = "teams")
     public String showAllTeams(Model model) {
         log.info("[/teams] GET endpoint invoked");
-        model.addAttribute("allteams", espnScrapeService.teamsExtractData());
+        model.addAttribute("allTeams", espnScrapeService.teamsExtractData());
         return "teams";
     }
 
